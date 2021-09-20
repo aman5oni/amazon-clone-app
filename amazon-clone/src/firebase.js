9280700672fa89a8f36905f00bd6e7de9315675e
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2021-09-20 15:02:24 +0530

    "commit

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAXT9PUSwZ_J_X_GODfVmkOIQ1QbaNh-LM",
+    authDomain: "clone-4726c.firebaseapp.com",
+    projectId: "clone-4726c",
+    storageBucket: "clone-4726c.appspot.com",
+    messagingSenderId: "17578314525",
+    appId: "1:17578314525:web:6aca118cc6b71f9849aff4",
+    measurementId: "G-163TVHEC1H"
+  };
