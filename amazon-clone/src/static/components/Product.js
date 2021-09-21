b3edfc68ec9fac1f8be2cb017e8b4181ad910447
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2021-09-21 19:06:29 +0530

    "checkout Basket Half Completed"

--- amazon-clone/src/static/components/Product.js
+++ amazon-clone/src/static/components/Product.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import '../css/Product.css';
-function Product({title,image,price,rating}) {
+import {useStateValue} from '../components/StateProvider'
+
+function Product({id,title,image,price,rating}) {
+    const [{basket},dispatch] = useStateValue();
+
+    console.log('this is the basket ==>',basket)
+
+    const addToBasket = () => {
+        dispatch({
+            type:'ADD_TO_BASKET',
+            item:{
+                id:id,
+                title:title,
+                image:image,
+                price:price,
+                rating:rating,
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product_info">
@@ -17,7 +36,7 @@ function Product({title,image,price,rating}) {
                 </div>
             </div>
             <img src={image}></img>
-            <button>Add To Basket</button>
+            <button onClick={addToBasket}>Add To Basket</button>
         </div>
     )
 }
