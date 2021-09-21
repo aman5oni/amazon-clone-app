cd56685e2e0302c7ac3938ce6a9f2d3dd6a6e336
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2021-09-21 15:31:19 +0530

    completed home page

--- amazon-clone/src/static/components/Product.js
+++ amazon-clone/src/static/components/Product.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import '../css/Product.css';
-function Product() {
+function Product({title,image,price,rating}) {
     return (
         <div className="product">
             <div className="product_info">
-                <p>the lean startup</p>
+                <p>{title}</p>
                 <p className="product_price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product_rating">
-                    <p>⭐</p>
-                    <p>⭐</p>
-                    <p>⭐</p>
+                    { Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                    <p>⭐</p>))}
                 </div>
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"></img>
+            <img src={image}></img>
             <button>Add To Basket</button>
         </div>
     )
