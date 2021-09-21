32b1ea50e5ffc3564b5389ee5ecc8da95bba6942
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2021-09-21 15:42:54 +0530

    .

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Header from './static/components/Header';
 import Home from './static/components/Home';
+
 function App() {
   return (
+    <Router>
     <div className="App">
-    <Header />
-    <Home/>
-    {/*home*/}
+      <Switch>
+      <Route path="/checkout">
+          <Header/>
+          <h1>HEllo I Am A Checkout</h1>
+        </Route>
+        <Route path="/">
+          <Header/>
+          <Home/>
+        </Route>
+       
+      </Switch>
     </div>
-    
+    </Router>
   );
 }
 
