a4a7e69984471e1198677d8c476c709a5f40060a
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2021-10-01 09:30:49 +0530

    "

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,9 +1,19 @@
+import { initializeApp} from "firebase/app";
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyAXT9PUSwZ_J_X_GODfVmkOIQ1QbaNh-LM",
-    authDomain: "clone-4726c.firebaseapp.com",
-    projectId: "clone-4726c",
-    storageBucket: "clone-4726c.appspot.com",
-    messagingSenderId: "17578314525",
-    appId: "1:17578314525:web:6aca118cc6b71f9849aff4",
-    measurementId: "G-163TVHEC1H"
-  };
+  apiKey: "AIzaSyAXT9PUSwZ_J_X_GODfVmkOIQ1QbaNh-LM",
+  authDomain: "clone-4726c.firebaseapp.com",
+  projectId: "clone-4726c",
+  storageBucket: "clone-4726c.appspot.com",
+  messagingSenderId: "17578314525",
+  appId: "1:17578314525:web:6aca118cc6b71f9849aff4",
+  measurementId: "G-163TVHEC1H"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const auth = getAuth();
+
+export { db, auth };
--- amazon-clone/src/static/components/Login.js
+++ amazon-clone/src/static/components/Login.js
@@ -1,8 +1,42 @@
-import React from 'react'
-import "../css/Login.css"
-import {Link} from "react-router-dom"
+import React,{useState} from 'react';
+import "../css/Login.css";
+import {Link,useHistory} from "react-router-dom";
+import { auth,db } from "../../firebase";
+
 
 function Login() {
+    const history = useHistory();
+
+    const [email,setEmail] = useState('');
+
+    const [password,setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
+
+
     return (
         <div className="login">
             <Link to="/">
@@ -12,14 +46,16 @@ function Login() {
                 <h1>Sign-in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text"/>
+                    <input type="text" value={email} onChange={ e => setEmail(e.target.value) } />
                     <h5>Password</h5>
-                    <input type="password"/>
-                    <button className="login_signInButton"> Sign In</button>
+                    <input type="password" value={password} onChange={ e => setPassword(e.target.value) } />
+                    <button className="login_signInButton" type="submit" onClick={signIn}> Sign In</button>
                     <p>
-                        By Signing-in you agree to the AMAZON-CLONE-APP Conditions Of Use & Sale. Please see our Privacy Notice & readme.md our cookies Notice and Our internet Based Ads Notice.
+                        <br/>
+                            By Signing-in you agree to the AMAZON-CLONE-APP Conditions Of Use & Sale. Please see our Privacy Notice & readme.md our cookies Notice and Our internet Based Ads Notice.
+                        <br/>
                     </p>
-                    <button className="login_registerButton">Create Your Amazon Account</button>
+                    <button className="login_registerButton" onClick={register} >Create Your Amazon Account</button>
                 </form>
             </div>
         </div>
